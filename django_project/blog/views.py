36ad373f208213f2cda24c452774cacf3de07317
Dixit052 <20bsm008@iiitdmj.ac.in>
2022-05-28 22:31:45 +0530

    update

---
+++ django_project/blog/views.py
@@ -0,0 +1,10 @@
+import imp
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    return render(request,'blog/home.html')
+
+
+def about(request):
+    return render(request,'blog/about.html')    
